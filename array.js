95cd607703275f6611d14d3d161de295128cfcf1
susthir <susthir19march@gmail.com>
2024-03-20 19:24:45 +0530

    object practice

---
+++ array.js
@@ -0,0 +1,12 @@
+let Colors = ["red","green","yellow","blue","cyan","magneta","sky","orange"]
+let ids =[12,13,14,15,45,67,65]
+// for(let color of Colors){
+//     console.log(color);
+// }
+// for(let i=0;i<=6;i++){
+//     console.log(ids[i]);
+// }
+for(let i=0;i<=ids.length-1;i++){
+    console.log(ids[i]);
+    console.log(ids.length);
+}
---
+++ object.js
@@ -0,0 +1,81 @@
+// let a = {};
+// let b = [];
+// console.log(typeof a);
+// console.log(typeof b);
+// let emp = {
+//   id: 101,
+//   name: "Roshan",
+//   age: 24,
+// };
+// console.log(emp);
+// //how to access object values
+// //objec.propertyname
+// console.log(emp.name);
+// console.log(emp.age);
+
+// let user = {
+//   id: 101,
+//   name: "Rahul",
+//   name: "Rahul Gandhi",
+//   email: "rahul@gmail.com",
+// };
+// console.log(user);
+// user.name = "Thyson";
+// console.log(user);
+
+// let myobj = {
+//   id: 1,
+//   name: "Leanne Graham",
+//   username: "Bret",
+//   email: "Sincere@april.biz",
+//   address: {
+//     street: "Kulas Light",
+//     suite: "Apt. 556",
+//     city: "Gwenborough",
+//     zipcode: "92998-3874",
+//     geo: {
+//       lat: "-37.3159",
+//       lng: "81.1496",
+//     },
+//   },
+//   phone: "1-770-736-8031 x56442",
+//   website: "hildegard.org",
+//   company: {
+//     name: "Romaguera-Crona",
+//     catchPhrase: "Multi-layered client-server neural-net",
+//     bs: "harness real-time e-markets",
+//   },
+// };
+// console.log(myobj);
+// console.log(myobj.company.name);
+// for(obj in myobj){
+// console.log(myobj[obj]);
+// }
+
+// let address ={
+//     street: "Kulas Light",
+//     suite: "Apt. 556",
+//     city: "Gwenborough",
+//     zipcode: "92998-3874"
+// }
+
+// for(key in address){
+//     console.log(address[key]);
+// }
+// let numbers =[90,87,67,56,7464,83]
+// for(num of numbers){
+// console.log(num);
+// }
+
+const details ={
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    phone : "7897894569"
+}
+console.log(details);
+for( det in details){
+    console.log(details[det]);
+    console.log(details.key);
+}
